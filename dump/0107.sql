619d45f00c888c0a8fd6644a6fa5c4c5a5b510f6
Kirillov Dmitrii <79657946+gmbk38@users.noreply.github.com>
2022-07-01 16:25:46 +0300

    Add files via upload

--- dump/0107.sql
+++ dump/0107.sql
@@ -25,7 +25,7 @@ DROP TABLE IF EXISTS `final_test`;
 CREATE TABLE `final_test` (
   `q` varchar(500) DEFAULT NULL,
   `a` varchar(500) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
