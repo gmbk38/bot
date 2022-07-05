f838e7ea37e1c8056960293fb03d8fafbb63200c
Dmitrii <gmbk38@yandex.ru>
2022-07-05 15:27:31 +0300

    12

--- logs.py
+++ logs.py
@@ -11,7 +11,7 @@ class User_log():
 
     def user_record(self):
         #добавляем лог пользователя
-        file_check = open("logs/users/users.txt","r",encoding='utf-8')
+        file_check = open("/home/start/bot/logs/users/users.txt","r",encoding='utf-8')
         lines = file_check.readlines()
         mark = 0
         for line in lines:
@@ -21,14 +21,14 @@ class User_log():
                 mark = 1
                 break
         if mark == 0:
-            file_upd = open("logs/users/users.txt","a+",encoding='utf-8')
+            file_upd = open("/home/start/bot/logs/users/users.txt","a+",encoding='utf-8')
             file_upd.write(str(self.id) + " | " + str(self.fname) + " | " + str(self.lname) + " | " + str(self.nickname) + "\n")
             file_upd.close()
 
 
     def chat_record(self,msg):
         #записываем сообщения пользователя
-        file_upd = open("logs/chats/" + str(self.id) + ".txt","a+",encoding='utf-8')
+        file_upd = open("/home/start/bot/logs/chats/" + str(self.id) + ".txt","a+",encoding='utf-8')
         date = str(datetime.now()).split(".")[0]
         file_upd.write(date + " " + msg + "\n")
         file_upd.close()
@@ -36,7 +36,7 @@ class User_log():
 
     def err_record(self,ex):
         #записываем ошибки
-        file_upd = open("logs/err/err.txt","a+",encoding='utf-8')
+        file_upd = open("/home/start/bot/logs/err/err.txt","a+",encoding='utf-8')
         date = str(datetime.now()).split(".")[0]
         file_upd.write(date + " " + str(self.id) + "\n")
         file_upd.write(str(ex) + "\n")
