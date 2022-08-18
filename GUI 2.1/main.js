1dde29a85c8ac5b1cfba23cbbe2f2df282064e32
Kirillov Dmitrii <gmbk38@yandex.ru>
2022-08-18 18:38:33 +0300

    upd

--- GUI 2.1/main.js
+++ GUI 2.1/main.js
@@ -5,18 +5,21 @@ var flag = false;
 
 var sort = document.getElementsByClassName("sort");
 button.addEventListener("click", e => {
-  console.log(e)
+  // console.log(e)
   if (flag === false) {
     flag = true;
     document.getElementById("try_to_hide").style.height = 0 + "vh";
+    document.getElementById("try_to_hide").style.width = 0 + "vw";
     document.getElementById("try_to_hide").style.marginTop = 0;
-    document.getElementById("tth2").style.opacity = 1;
-    // document.getElementById("tth2").style.display = "block";
+    document.getElementById("try_to_hide").style.left = 10 + "vw";
+    document.getElementById("try_to_hide").style.padding = 0;
   } else {
     flag = false;
     document.getElementById("try_to_hide").style.height = 40 + "vh";
+    document.getElementById("try_to_hide").style.width = 70 + "vw";
     document.getElementById("try_to_hide").style.marginTop = 1.5 + "%";
-    document.getElementById("tth2").style.opacity = 0;
+    document.getElementById("try_to_hide").style.left = 50 + "%";
+    document.getElementById("try_to_hide").style.padding = 1.5 + "vw";
   }
 //   document.getElementById("try_to_hide").style.display = "none";
 });
