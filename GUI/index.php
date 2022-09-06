bec0ec7d00b7010c78a175cb77800f0c962e1371
Dmitrii <gmbk38@yandex.ru>
2022-09-06 12:49:33 +0300

    Update index.php

--- GUI/index.php
+++ GUI/index.php
@@ -1,13 +1,14 @@
 <?php
-$dbname = 'test';
-$dbuser = 'root';
-$dbpass = 'Gorizont22';
-$dbhost = 'localhost';
-$connect = mysql_connect($dbhost, $dbuser, $dbpass) or die("Unable to connect to '$dbhost'");
-mysql_select_db($dbname) or die("Could not open the database '$dbname'");
-$result = mysql_query("SELECT * FROM final_test");
-while ($row = mysql_fetch_array($result, MYSQL_NUM)) {
-    printf("ID: %s  Name: %s <br>", $row[0], $row[1]);
-}
+$servername = "localhost";
+$username = "root";
+$password = "Gorizont22";
 
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
 ?>
