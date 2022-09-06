5750491a72b46fe7a97fd9d20f817a2a53075fb4
Dmitrii <gmbk38@yandex.ru>
2022-09-06 09:30:13 +0300

    DB update

--- dump_conv.py
+++ dump_conv.py
@@ -1,6 +1,7 @@
 import pandas
 
-excel_data_df = pandas.read_excel('new data 3.xlsx', sheet_name='БД 2ВО')
+excel_data_df = pandas.read_excel('data0609.xlsx', sheet_name='БД 2ВО')
+excel_data_2 = pandas.read_excel('data0609.xlsx', sheet_name='БД Соц жизнь')
 
 # print(excel_data_df)
 
@@ -10,13 +11,24 @@ excel_data_df = pandas.read_excel('new data 3.xlsx', sheet_name='БД 2ВО')
 # print(excel_data_df.to_dict(orient='record'))
 
 dict_array = excel_data_df.to_dict(orient='record')
+dict_2 = excel_data_2.to_dict(orient='record')
 
 dump_input = ""
 
 for dict in dict_array:
     stroka = "("
     for key in dict:
-        stroka += "'" + str(dict[key]) + "'" + ','
+        if "syn" not in key:
+            stroka += "'" + str(dict[key]) + "'" + ','
+    stroka = stroka[:-1]
+    stroka += "),"
+    dump_input += stroka
+
+for dict in dict_2:
+    stroka = "("
+    for key in dict:
+        if "syn" not in key:
+            stroka += "'" + str(dict[key]) + "'" + ','
     stroka = stroka[:-1]
     stroka += "),"
     dump_input += stroka
