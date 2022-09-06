11688c143a882214fc7c357cb141bfa537e50c88
Dmitrii <gmbk38@yandex.ru>
2022-09-06 14:44:22 +0300

    Update index.php

--- GUI/index.php
+++ GUI/index.php
@@ -5,8 +5,6 @@ $mysqli = new mysqli("localhost","gui","gui123","test");
 if ($mysqli -> connect_errno) {
   echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
   exit();
-} else {
-    echo "OK";
 }
 
 $sql = "SELECT * FROM `final_test`;";
