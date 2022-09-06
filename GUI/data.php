ec88e0d2645ff3247635966af4a8a6bd689b29e7
Dmitrii <gmbk38@yandex.ru>
2022-09-06 12:39:13 +0300

    upd

--- GUI/data.php
+++ GUI/data.php
@@ -1,5 +1,13 @@
 <?php
-if( isset( $_POST['my_button'] ) )
-echo 'Нажата кнопка my_button';
+$dbname = 'test';
+$dbuser = 'root';
+$dbpass = 'Gorizont22';
+$dbhost = 'localhost';
+$connect = mysql_connect($dbhost, $dbuser, $dbpass) or die("Unable to connect to '$dbhost'");
+mysql_select_db($dbname) or die("Could not open the database '$dbname'");
+$result = mysql_query("SELECT * FROM final_test");
+while ($row = mysql_fetch_array($result, MYSQL_NUM)) {
+    printf("ID: %s  Name: %s <br>", $row[0], $row[1]);
+}
 
 ?>
