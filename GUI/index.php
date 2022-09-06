43ff4cd0a56090c1a092b909f568625e3abf9e63
Dmitrii <gmbk38@yandex.ru>
2022-09-06 13:08:28 +0300

    Update index.php

--- GUI/index.php
+++ GUI/index.php
@@ -1,21 +1,9 @@
 <?php
-function OpenCon()
- {
- $dbhost = "localhost:3306";
- $dbuser = "root";
- $dbpass = "Gorizont22";
- $db = "test";
- $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
- 
- echo($conn);
- return $conn;
- }
- 
-function CloseCon($conn)
- {
- $conn -> close();
- }
-
- OpenCon();
-   
+$dbhost = "localhost:3306";
+$dbuser = "root";
+$dbpass = "Gorizont22";
+$db = "test";
+$conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
+  
+echo($conn);
 ?>
