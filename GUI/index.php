5a01173b7508d41f3eb16abef74d0a3ae74c0801
Dmitrii <gmbk38@yandex.ru>
2022-09-06 14:03:23 +0300

    Update index.php

--- GUI/index.php
+++ GUI/index.php
@@ -7,17 +7,10 @@ if ($mysqli -> connect_errno) {
   exit();
 } else {
     echo "OK";
-}
 
-$sql = "SELECT * FROM final_test";
-$result = $mysqli -> query($sql);
-echo($result);
-// Fetch all
-$result -> fetch_all(MYSQLI_ASSOC);
-echo($result);
-// Free result set
-$result -> free_result();
-echo($result);
-$mysqli -> close();
-echo($result);
+    $sql = "SELECT * FROM final_test";
+    $result = $mysqli -> query($sql);
+    echo($result);
+    echo "OK";
+}
 ?>
