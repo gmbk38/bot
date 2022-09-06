0274671a082a370057da78f58006329eb1808168
Dmitrii <gmbk38@yandex.ru>
2022-09-06 14:41:40 +0300

    Update index.php

--- GUI/index.php
+++ GUI/index.php
@@ -19,7 +19,13 @@ $row = $result -> fetch_all(MYSQLI_ASSOC);
 
 for ($i = 0; $i < count($row); $i++) {
     $e = $row[$i];
-    printf ("%s &nbsp;&nbsp;&nbsp;&nbsp; %s &nbsp;&nbsp;&nbsp;&nbsp; %s &nbsp;&nbsp;&nbsp;&nbsp; %s", $e["id"], $e["tag"], $e["q"], $e["a"]);
+    printf ("%s 
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
+    %s 
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
+    %s 
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
+    %s ", $e["id"], $e["tag"], $e["q"], $e["a"]);
     echo "<br>";
     echo "<br>";
     echo "<br>";
