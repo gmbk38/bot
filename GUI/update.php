69b468c147405d4d08e095e1f0d4be74bfc7082b
Kirillov Dmitrii <gmbk38@yandex.ru>
2022-09-08 16:46:06 +0300

    Update update.php

--- GUI/update.php
+++ GUI/update.php
@@ -8,21 +8,21 @@ $q = $_POST["q"];
 $exa = $_POST["exa"];
 $a = $_POST["a"];
 
-$mysqli = new mysqli("localhost","gui","gui123","test");
+// $mysqli = new mysqli("localhost","gui","gui123","test");
 
-if ($mysqli -> connect_errno) {
-  echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
-  exit();
-}
+// if ($mysqli -> connect_errno) {
+//   echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+//   exit();
+// }
 
-alert("data upd");
-$sql = "UPDATE `final_test` SET id = $id, tag = $tag, q = $q, a = $a WHERE id = $exid AND tag = $extag AND q = $exq AND a = $exa;";
-$result = $mysqli -> query($sql);
-alert("success data upd");
+// alert("data upd");
+// $sql = "UPDATE `final_test` SET id = $id, tag = $tag, q = $q, a = $a WHERE id = $exid AND tag = $extag AND q = $exq AND a = $exa;";
+// $result = $mysqli -> query($sql);
+// alert("success data upd");
 
-$row = $result -> fetch_all(MYSQLI_ASSOC);
+// $row = $result -> fetch_all(MYSQLI_ASSOC);
 
-$result -> free_result();
+// $result -> free_result();
 
-$mysqli -> close();
+// $mysqli -> close();
 ?>
