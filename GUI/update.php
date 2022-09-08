4a258815dfd024ba26439a68aa4458304263ab1f
Kirillov Dmitrii <gmbk38@yandex.ru>
2022-09-08 16:06:45 +0300

    Update update.php

--- GUI/update.php
+++ GUI/update.php
@@ -1,11 +1,4 @@
 <?php
-$mysqli = new mysqli("localhost","gui","gui123","test");
-
-if ($mysqli -> connect_errno) {
-  echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
-  exit();
-}
-
 $exid = $_POST["exid"]; 
 $id = $_POST["id"];
 $extag = $_POST["extag"];
@@ -14,6 +7,13 @@ $exq = $_POST["exq"];
 $q = $_POST["q"];
 $exa = $_POST["exa"];
 $a = $_POST["a"];
+
+$mysqli = new mysqli("localhost","gui","gui123","test");
+
+if ($mysqli -> connect_errno) {
+  echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+  exit();
+}
 
 alert("data upd")
 $sql = "UPDATE `final_test` SET id = $id, tag = $tag, q = $q, a = $a WHERE id = $exid, tag = $extag, q = $exq, a = $exa;";
