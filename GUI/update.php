3e43dc434c7c1116364cdd1d853688ae010c1b75
Kirillov Dmitrii <gmbk38@yandex.ru>
2022-09-08 17:31:02 +0300

    Update update.php

--- GUI/update.php
+++ GUI/update.php
@@ -16,7 +16,7 @@ if ($mysqli -> connect_errno) {
 }
 
 echo($exa);
-$sql = "UPDATE `final_test` SET id = '$id', tag = '$tag', q = '$q', a = '$a' WHERE id = '$exid' AND tag = '$extag' AND q = '$exq';";
+// $sql = "UPDATE `final_test` SET id = '$id', tag = '$tag', q = '$q', a = '$a' WHERE id = '$exid' AND tag = '$extag' AND q = '$exq';";
 
 // $sql = "SELECT * FROM `final_test` WHERE id = '$exid' AND tag = '$extag' AND q = '$exq';";
 $result = $mysqli -> query($sql);
