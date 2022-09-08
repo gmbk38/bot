4c86d4462f12199952a74003d01e85b39bcc8b51
Dmitrii <gmbk38@yandex.ru>
2022-09-08 10:58:01 +0300

    Update db.js

--- GUI/db.js
+++ GUI/db.js
@@ -32,6 +32,8 @@ $(document).ready(function() {
         var test_for_show = row;
         console.log(test_for_show);
         console.log(test_for_show.nodeName);
+        test_for_show = str(test_for_show);
+        console.log(test_for_show.nodeName);
       });
   });
 });
