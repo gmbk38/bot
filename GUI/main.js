96eeaa5d5fbc6b1073d4a3ce56b3066258d1ad54
Kirillov Dmitrii <gmbk38@yandex.ru>
2022-09-08 14:04:34 +0300

    Update main.js

--- GUI/main.js
+++ GUI/main.js
@@ -639,6 +639,7 @@ $(document).ready(function() {
     var element = $(this).val();
     element = String(element);
     element = element.split(" ");
+    console.log(element);
     main_data.push(element);
   });
   for (data in php_row) {
