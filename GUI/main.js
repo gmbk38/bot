31933de2c953ddf64c5bf482d63a860ec1a64551
Kirillov Dmitrii <gmbk38@yandex.ru>
2022-09-08 15:02:15 +0300

    Update main.js

--- GUI/main.js
+++ GUI/main.js
@@ -654,7 +654,7 @@ $(document).ready(function() {
   //     "</tr>"
   //   );
   // };
-    for (var i = 50; i < 100; i++) {
+    for (var i = 70; i < 100; i++) {
     $("tr").last().after(
       "<tr>" +
         "<td>" + main_data[i][0] + " </td> " +
