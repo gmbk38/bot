285fdba5345f5cbe77edb18844bb242b437fc092
Kirillov Dmitrii <gmbk38@yandex.ru>
2022-09-08 16:14:32 +0300

    Update update copy.php

--- GUI/update copy.php
+++ GUI/update copy.php
@@ -1,4 +1,5 @@
 <?php
+
 $test = $_POST['test'];
 if ($test == '') {
     echo("NO DATA");
@@ -6,5 +7,12 @@ if ($test == '') {
     echo("OK");
     echo($test);
     echo("OK");
+}
+
+$mysqli = new mysqli("localhost","gui","gui123","test");
+
+if ($mysqli -> connect_errno) {
+  echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+  exit();
 }
 ?>
